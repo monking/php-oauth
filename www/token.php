b5099961b795b3f3f022450a3561bb522543f897
François Kooman <fkooman@tuxed.net>
2012-07-29 17:14:07 +0200

    add error reporting to token endpoint

--- www/token.php
+++ www/token.php
@@ -7,37 +7,54 @@ require_once "../lib/Http/HttpRequest.php";
 require_once "../lib/Http/HttpResponse.php";
 require_once "../lib/Http/IncomingHttpRequest.php";
 
-$config = new Config(dirname(__DIR__) . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "oauth.ini");
-
-$oauthStorageBackend = $config->getValue('storageBackend');
-require_once "../lib/OAuth/$oauthStorageBackend.php";
-$storage = new $oauthStorageBackend($config);
-
-$authorizationServer = new AuthorizationServer($storage, $config);
-
-$incomingRequest = new IncomingHttpRequest();
-$request = $incomingRequest->getRequest();
 $response = new HttpResponse();
 
-switch($request->getRequestMethod()) {
+try { 
+    $config = new Config(dirname(__DIR__) . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "oauth.ini");
 
-    case "POST":
-        try {
+    $oauthStorageBackend = $config->getValue('storageBackend');
+    require_once "../lib/OAuth/$oauthStorageBackend.php";
+    $storage = new $oauthStorageBackend($config);
+
+    $authorizationServer = new AuthorizationServer($storage, $config);
+
+    $incomingRequest = new IncomingHttpRequest();
+    $request = $incomingRequest->getRequest();
+
+
+    switch($request->getRequestMethod()) {
+
+        case "POST":
             $result = $authorizationServer->token($request->getPostParameters(), $request->getBasicAuthUser(), $request->getBasicAuthPass());
             $response->setHeader('Content-Type', 'application/json');
             $response->setHeader('Cache-Control', 'no-store');
             $response->setHeader('Pragma', 'no-cache');
             $response->setContent(json_encode($result));
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
-        break;
+            break;
 
-    default: 
-        // method not allowed
-        $response->setStatusCode(405);
-        $response->setHeader("Allow", "POST");
-        break;
+        default: 
+            // method not allowed
+            $response->setStatusCode(405);
+            $response->setHeader("Allow", "POST");
+            break;
+    }
+} catch (Exception $e) {
+    switch(get_class($e)) {
+        case "TokenException":
+            if($e->getResponseCode() === 401) {
+                $response->setHeader("WWW-Authenticate", 'Basic realm="OAuth Server"');
+            }
+            $response->setStatusCode($e->getResponseCode());
+            $response->setHeader('Content-Type', 'application/json');
+            $response->setHeader('Cache-Control', 'no-store');
+            $response->setHeader('Pragma', 'no-cache');
+            $response->setContent(json_encode(array("error" => $e->getMessage(), "error_description" => $e->getDescription())));
+            break;
+
+        default:
+            $response->setStatusCode(500);
+            $response->setContent(json_encode(array("error" => $e->getMessage())));
+    }
 }
 
 $response->sendResponse();
