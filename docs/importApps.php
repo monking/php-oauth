c4a0f678c6e920b04fc85215516ddaeff1290e0b
F. Kooman <fkooman@tuxed.net>
2012-09-12 01:00:15 +0200

    fix import script

--- docs/importApps.php
+++ docs/importApps.php
@@ -6,6 +6,7 @@ $c->register();
 
 use \Tuxed\Config as Config;
 use \Tuxed\OAuth\PdoOAuthStorage as PdoOAuthStorage;
+use \Tuxed\OAuth\ClientRegistration as ClientRegistration;
 
 $config = new Config(dirname(__DIR__) . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "oauth.ini");
 
@@ -18,11 +19,6 @@ if($argc !== 2) {
 }
 
 $manifestFile = $argv[1];
-#if(!file_exists($manifestFile) || !is_file($manifestFile) || !is_readable($manifestFile)) {
-#        echo "unable to read manifest file." . PHP_EOL;
-#        die();
-#}
-
 $fileContents = file_get_contents($manifestFile);
 $data = json_decode($fileContents, TRUE);
 if(NULL === $data || !is_array($data)) {
@@ -41,10 +37,11 @@ foreach($data as $d) {
                         "secret" => NULL,
                         "type" => "user_agent_based_application",
                         "icon" => $d['icons']['128'],
-			"allowed_scope" => implode(" ", $d['permissions']),
+			            "allowed_scope" => implode(" ", $d['permissions']),
                         "redirect_uri" => $d['app']['launch']['web_url'],
                 );
-                $storage->addClient($x);
+                $y = ClientRegistration::fromArray($x);
+                $storage->addClient($y->getClientAsArray());
         }
 }
 
